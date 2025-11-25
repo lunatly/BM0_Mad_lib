5b13ee77b974b91a1df299cfd3d2549de861c5bb
danialdol <danialdol@nycstudents.net>
2025-11-25 11:48:51 -0500

    Update bm0_madlibs.py

--- bm0_madlibs.py
+++ bm0_madlibs.py
@@ -1,2 +1,6 @@
-print('hello world') 
+verb = str(input('please enter a verb(action) word : '))
+noun = str(input('please enter a noun(person,place,or thing) word : '))
+adj = str(input('please enter a adjective(describes) word : '))
 
+print(f'today i decided to {verb} to the park')
+print(f'when i arrrived i saw a very {adj} {noun} sitting on the bench')
